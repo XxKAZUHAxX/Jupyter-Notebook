9b064eb0533060b919ef5e291e4e06dc75d2d92b
XxKAZUHAxX <jesryljade18@gmail.com>
2023-02-23 22:32:37 +0800

    modified:   Python Scripts/drawing_w_mouse.py

--- Python Scripts/drawing_w_mouse.py
+++ Python Scripts/drawing_w_mouse.py
@@ -1,3 +1,17 @@
 import cv2
 import numpy as np
 
+def draw_cicle(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(img=img, center=(x, y), radius=50, color=(0, 255, 0), thickness=-1)
+
+cv2.namedWindow('image')
+cv2.setMouseCallback('image', draw_cicle)
+
+img = np.zeros((1000, 1000, 3), np.uint8)
+
+while True:
+    cv2.imshow('image', img)
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+cv2.destroyAllWindows()

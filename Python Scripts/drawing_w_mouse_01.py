244ae6c1ef4c2963827fd3b15ed7cf712a67913c
XxKAZUHAxX <jesryljade18@gmail.com>
2023-02-25 15:39:05 +0800

    modified:   Python Scripts/drawing_w_mouse_01.py

--- Python Scripts/drawing_w_mouse_01.py
+++ Python Scripts/drawing_w_mouse_01.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def draw_circle(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(img=img, center=(x, y), radius=50, color=(0, 255, 0), thickness=-1)
+        cv2.circle(img=img, center=(x, y), radius=25, color=(0, 255, 0), thickness=-1)
 
 cv2.namedWindow('image')
 cv2.setMouseCallback('image', draw_circle)
